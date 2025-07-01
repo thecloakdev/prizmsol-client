9765ccc1867771c7aa272010f50fff2b3d359813
Ahmed Ahmed <ahmed23.ahmed@gmail.com>
2025-07-02 02:56:12 +0400

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
-import { type NextRequest } from 'next/server';
-import RedirectMiddleware from './middlewares/redirect';
+import { NextResponse, type NextRequest } from 'next/server';
+import { getValidSubdomain } from './lib/subdomain';
+
+const PUBLIC_FILE = /\.(.*)$/; // Files
 
 export async function middleware(request: NextRequest) {
-    return RedirectMiddleware(request);
+    // Clone the URL
+    const url = request.nextUrl.clone();
+
+    // Skip public files
+    if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes("_next"))
+        return;
+
+    const host = request.headers.get("host") as string;
+    const subdomain = getValidSubdomain(host);
+    if (subdomain && subdomain !== "www" && subdomain !== "prizmsol") {
+        url.pathname = `/site/${subdomain}${url.pathname}`;
+        return NextResponse.rewrite(url);
+    }
 }
